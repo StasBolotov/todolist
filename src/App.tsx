c9a8591dfb2c05084c31e7604f08d840125cf04b
Stas <Stashek.web.404@gmail.com>
2024-04-17 13:07:09 +0300

    Урок-1. разобрал map

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,23 @@ import './App.css';
 import {ToDoList} from "./ToDoList";
 
 function App() {
+    const task1 = [
+        {id:1, title: 'HTML&CSS', isDone: true},
+        {id:2, title: 'JS', isDone: false},
+        {id:3, title: 'ReactJS', isDone: true},
+        {id:4, title: 'Redux', isDone: true}
+    ]
+    const task2 = [
+        {id:1, title: 'Hello world', isDone: false},
+        {id:2, title: 'I am Happy', isDone: true},
+        {id:3, title: 'Yo', isDone: false},
+        {id:4, title: 'Redux', isDone: false}
+    ]
+
     return (
-        <div>
-            <ToDoList/>
-            <ToDoList/>
-            <ToDoList/>
+        <div className="App">
+            <ToDoList title="Задача1" tasks={task1} data={'17.04.2024'}/>
+            <ToDoList title="Задача2" tasks={task2}/>
         </div>
 
     );
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,24 +1,43 @@
 import React from "react";
 
-export const ToDoList = () => {
+type TasksType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+type PropsType = {
+    title: string
+    tasks: TasksType[]
+    data?: string
+}
+export const ToDoList = ({title, tasks, data}: PropsType) => {
     return (
         <div className="App">
             <div>
-                <h3>What to learn</h3>
+                <h3>{title}</h3>
                 <div>
                     <input/>
                     <button>+</button>
                 </div>
                 <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                    {/*<li><input type="checkbox" checked={tasks[0].isDone}/> <span>{tasks[0].title}</span></li>*/}
+                    {/*<li><input type="checkbox" checked={tasks[1].isDone}/> <span>{tasks[1].title}</span></li>*/}
+                    {/*<li><input type="checkbox" checked={tasks[2].isDone}/> <span>{tasks[2].title}</span></li>*/}
+                    {tasks.map(task => {
+                        return (
+                            <li key={task.id}>
+                                <input type="checkbox" checked={task.isDone}/>
+                                <span>{task.title}</span>
+                            </li>
+                        )
+                    })}
                 </ul>
                 <div>
                     <button>All</button>
                     <button>Active</button>
                     <button>Completed</button>
                 </div>
+                <div>{data}</div>
             </div>
         </div>
     )
