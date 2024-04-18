f69e07cce96245b2457871c816dab7f297c09d39
Stas <Stashek.web.404@gmail.com>
2024-04-18 15:57:31 +0300

    Урок-1. Тернарный оператор

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ function App() {
         {id:3, title: 'ReactJS', isDone: true},
         {id:4, title: 'Redux', isDone: true}
     ]
-    const task2 = [
-        {id:1, title: 'Hello world', isDone: false},
-        {id:2, title: 'I am Happy', isDone: true},
-        {id:3, title: 'Yo', isDone: false},
-        {id:4, title: 'Redux', isDone: false}
+    const task2:any = [
+    //     {id:1, title: 'Hello world', isDone: false},
+    //     {id:2, title: 'I am Happy', isDone: true},
+    //     {id:3, title: 'Yo', isDone: false},
+    //     {id:4, title: 'Redux', isDone: false}
     ]
 
     return (
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -19,19 +19,22 @@ export const ToDoList = ({title, tasks, data}: PropsType) => {
                     <input/>
                     <button>+</button>
                 </div>
-                <ul>
-                    {/*<li><input type="checkbox" checked={tasks[0].isDone}/> <span>{tasks[0].title}</span></li>*/}
-                    {/*<li><input type="checkbox" checked={tasks[1].isDone}/> <span>{tasks[1].title}</span></li>*/}
-                    {/*<li><input type="checkbox" checked={tasks[2].isDone}/> <span>{tasks[2].title}</span></li>*/}
-                    {tasks.map(task => {
-                        return (
-                            <li key={task.id}>
-                                <input type="checkbox" checked={task.isDone}/>
-                                <span>{task.title}</span>
-                            </li>
-                        )
-                    })}
-                </ul>
+
+                {tasks.length === 0 ? (
+                    <p>Тасок нет</p>
+                ) : (
+                    <ul>
+                        {tasks.map(task => {
+                            return (
+                                <li key={task.id}>
+                                    <input type="checkbox" checked={task.isDone}/>
+                                    <span>{task.title}</span>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                )}
+
                 <div>
                     <button>All</button>
                     <button>Active</button>
